687a59b0353cf75e2140bf5573e99bf64017f59c
Rogith <rogithy@gmail.com>
2021-03-23 20:46:08 +0530

    first commit

---
+++ precache-manifest.7b5e5497204be6da5536d5b4b3749ba1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aec23c8393300e7a6bbd",
+    "url": "/static/js/main.aec23c83.chunk.js"
+  },
+  {
+    "revision": "8a7b2023effcee8fa950",
+    "url": "/static/js/1.8a7b2023.chunk.js"
+  },
+  {
+    "revision": "aec23c8393300e7a6bbd",
+    "url": "/static/css/main.6eff9583.chunk.css"
+  },
+  {
+    "revision": "54e8320c9cb7f506bbbab7801d30bff5",
+    "url": "/index.html"
+  }
+];
